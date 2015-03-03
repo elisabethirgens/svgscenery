2efa9309842d3eaffe35efbafb3f71b5ce468b1d
elisabethirgens <hello@elisabethirgens.com>
2015-03-03 13:47:44 +0100

    wrote some clouds

--- svgsprell.js
+++ svgsprell.js
@@ -1,7 +1,7 @@
 var shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 shape.setAttribute("cx", 800);
-shape.setAttribute("cy", 200);
+shape.setAttribute("cy", 100);
 shape.setAttribute("r", 50);
-shape.setAttribute("fill", "yellow");
+shape.setAttribute("fill", "orange");
 var popcorn = document.getElementById("popcorn");
 popcorn.appendChild(shape);
