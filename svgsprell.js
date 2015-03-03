261a8f41a1052b8e40b9caf37615fbe67312101e
elisabethirgens <hello@elisabethirgens.com>
2015-03-03 13:20:39 +0100

    made a landscape

--- svgsprell.js
+++ svgsprell.js
@@ -1,7 +1,7 @@
 var shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-shape.setAttribute("cx", 125);
-shape.setAttribute("cy", 125);
+shape.setAttribute("cx", 800);
+shape.setAttribute("cy", 200);
 shape.setAttribute("r", 50);
-shape.setAttribute("fill", "pink");
+shape.setAttribute("fill", "yellow");
 var popcorn = document.getElementById("popcorn");
 popcorn.appendChild(shape);
