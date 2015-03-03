9e2b2731e2c16894298e502e57fa52dd5cf9f284
elisabethirgens <hello@elisabethirgens.com>
2015-03-03 12:50:10 +0100

    created a pink circle

--- svgsprell.js
+++ svgsprell.js
@@ -1,1 +1,7 @@
-console.log ("hello");
+var shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+shape.setAttribute("cx", 125);
+shape.setAttribute("cy", 125);
+shape.setAttribute("r", 50);
+shape.setAttribute("fill", "pink");
+var popcorn = document.getElementById("popcorn");
+popcorn.appendChild(shape);
