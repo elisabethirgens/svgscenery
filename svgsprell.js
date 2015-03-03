d4434316076704ef49fe9f500ab0bbde5bf1ac3a
Helge Skogly Holm <helge.holm@gmail.com>
2015-03-03 16:49:18 +0100

    Normalized cloud shape positions and randomized scale.

--- svgsprell.js
+++ svgsprell.js
@@ -21,16 +21,23 @@ function createCircle(cx, cy, r) {
 function insertcloud(parentID) {
   var cloud = document.createElementNS("http://www.w3.org/2000/svg", "g");
   cloud.setAttribute("fill", "white");
-  var x = randy.randInt(-200, 900);
-  var y = randy.randInt(-100, 350);
-  cloud.setAttribute("transform", "translate(" + x + " " + y + ")");
 
-  cloud.appendChild(createCircle(randy.randInt(90, 120), randy.randInt(40, 80), randy.randInt(40, 60)));
-  cloud.appendChild(createCircle(randy.randInt(160, 190), randy.randInt(40, 80), randy.randInt(40, 60)));
-  cloud.appendChild(createCircle(randy.randInt(30, 80), randy.randInt(90, 100), randy.randInt(50, 60)));
-  cloud.appendChild(createCircle(randy.randInt(120, 160), randy.randInt(90, 100), randy.randInt(70, 75)));
-  cloud.appendChild(createCircle(randy.randInt(200, 250), randy.randInt(90, 100), randy.randInt(50, 60)));
-  cloud.appendChild(createCircle(randy.randInt(20, 250), randy.randInt(40, 100), randy.randInt(20, 40)));
+  var x = randy.randInt(0, 1000);
+  var y = randy.randInt(0, 400);
+  var translation = "translate(" + x + " " + y + ")";
+
+  var scaleX = randy.randInt(0.9, 1.1);
+  var scaleY = randy.randInt(0.9, 1.1);
+  var scale = "scale(" + scaleX + " " + scaleY + ")";
+
+  cloud.setAttribute("transform", translation + " " + scale);
+
+  cloud.appendChild(createCircle(randy.randInt(-50, -20), randy.randInt(-30, 10), randy.randInt(40, 60)));
+  cloud.appendChild(createCircle(randy.randInt(20, 50), randy.randInt(-30, 10), randy.randInt(40, 60)));
+  cloud.appendChild(createCircle(randy.randInt(-110, -60), randy.randInt(20, 30), randy.randInt(50, 60)));
+  cloud.appendChild(createCircle(randy.randInt(-20, 20), randy.randInt(20, 30), randy.randInt(70, 75)));
+  cloud.appendChild(createCircle(randy.randInt(60, 110), randy.randInt(20, 30), randy.randInt(50, 60)));
+  cloud.appendChild(createCircle(randy.randInt(-110, 110), randy.randInt(-30, 30), randy.randInt(20, 40)));
 
   var cloudContainer = document.getElementById(parentID);
   cloudContainer.appendChild(cloud);
