ca5e6b3f50a147d7d263ddfa1192bcb605b179e8
Helge Skogly Holm <helge.holm@gmail.com>
2015-03-03 13:45:10 +0100

    use calc.getSunHeight to set sun pos

--- svgsprell.js
+++ svgsprell.js
@@ -1,6 +1,6 @@
 var shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 shape.setAttribute("cx", 800);
-shape.setAttribute("cy", 200);
+shape.setAttribute("cy", calc.getSunHeight());
 shape.setAttribute("r", 50);
 shape.setAttribute("fill", "yellow");
 var popcorn = document.getElementById("popcorn");
